87ed588c8593ebcef4a2e388dbf703e88accad9a
Toshio Ito <debug.ito@gmail.com>
2013-07-28 22:20:35 +0900

    local_server.js: add message log in Connection and Server. This is very handy for testing.

--- js/local_server.js
+++ js/local_server.js
@@ -9,10 +9,13 @@ jQluster ||= {};
     my.ConnectionLocal = function(server) {
         this.server = server;
         this.receive_callbacks = [];
+        this.send_log = [];
+        this.receive_log = [];
     };
     my.ConnectionLocal.prototype = {
         send: function(message) {
             // @return: nothing
+            this.send_log.push(my.clone(message));
             if(message.message_type === "register") {
                 this.server.register(this, message.body.remote_id);
             }else {
@@ -31,14 +34,19 @@ jQluster ||= {};
 
         triggerReceive: function(message) {
             // @return: nothing
+            this.receive_log.push(my.clone(message));
             $.each(this.receive_callbacks, function(i, callback) {
                 callback(message);
             });
-        }
+        },
+
+        getSendLog: function() { return this.send_log; },
+        getReceiveLog: function() { return this.receive_log; }
     };
     
     my.ServerLocal = function() {
         this.connections = {};
+        this.register_log = [];
     };
     my.ServerLocal.prototype = {
         register: function(connection, remote_id) {
@@ -46,6 +54,7 @@ jQluster ||= {};
             if(!this.connections[remote_id]) {
                 this.connections[remote_id] = [];
             }
+            this.register_log.push(remote_id);
             this.connections[remote_id].push(connection);
             this.distribute({
                 message_id: my.uuid(),
@@ -60,9 +69,11 @@ jQluster ||= {};
             var conn_list = this.connections[message.to];
             if(!conn_list) return;
             $.each(conn_list, function(i, conn) {
-                var dup_message = $.extend(true, {}, message);
+                var dup_message = my.clone(message);
                 conn.triggerReceive(dup_message);
             });
-        }
+        },
+
+        getRegisterLog: function() { return this.register_log; };
     };
 })(jQluster, jQuery);
