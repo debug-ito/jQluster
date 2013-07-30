61c0b6a597db07f11a2387cd19893d758122a7c7
Toshio Ito <debug.ito@gmail.com>
2013-07-30 21:43:35 +0900

    transport.js: add specification about some edge cases.

--- js/transport.js
+++ js/transport.js
@@ -29,11 +29,26 @@ if(!jQluster) { var jQluster = {}; }
         selectAndGet: function(args) {
             // @params: args.eval_code, args.remote_id
             // @return: Deferred that contains the obtained data
+            // 
+            // If the remote node does not exist, the request is
+            // deferred and delivered to the remote node when it
+            // appears. If there are multiple remote nodes, the
+            // result is not defined (for now).
         },
         selectAndListen: function(args) {
             // @params: args.eval_code, args.method, args.options = {},
             //          args.callback, args.remote_id
-            // @return: nothing (for now)
+            // @return: Deferred, whose content is meaningless.
+            // 
+            // If the remote node does not exist, the 'listen' request
+            // is deferred and delivered to the remote node when it
+            // appears. If there are multiple remote nodes, all of
+            // them receive the listen request.
+            //
+            // A node keeps only one callback for one "listen"
+            // request. If a node receives multiple listen requests
+            // with the same signature, only the last received one is
+            // valid. Callbacks for older requests are discarded.
         },
         
         _onReceive: function(message) {
