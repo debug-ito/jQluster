eae53ad81b5b023c83199a5d8351713829b0866b
Toshio Ito <debug.ito@gmail.com>
2013-09-08 21:27:58 +0900

    jQluster.htmlEscape() is refactored into js/lib/myutil.js, because the function is not required by jQluster code.

--- js/util.js
+++ js/util.js
@@ -59,17 +59,4 @@ if(!jQluster) { var jQluster = {}; }
                 // : my.quoteString(arg)
         }).join(",");
     };
-
-    // ** http://stackoverflow.com/questions/5499078/fastest-method-to-escape-html-tags-as-html-entities
-    var ESCAPE_CHARS = {
-        '&': '&amp;',
-        '<': '&lt;',
-        '>': '&gt;'
-    };
-    var _escape =  function(c) {
-        return ESCAPE_CHARS[c] || c;
-    };
-    my.htmlEscape = function(str) {
-        return str.replace(/[&<>]/g, _escape);
-    };
 })(jQluster);
