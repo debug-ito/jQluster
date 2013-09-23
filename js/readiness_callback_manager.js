bfd33fa7cb76e5b74bd4f9f911bcccf7ced2ebc8
Toshio Ito <debug.ito@gmail.com>
2013-09-23 10:39:23 +0900

    Refactor readiness callback management in RemoteSelectorFactory into ReadinessCallbackManager. A factory delegates readiness callback handling to the manager.

---
+++ js/readiness_callback_manager.js
@@ -0,0 +1,87 @@
+"use strict";
+
+// jQluster readiness callback manager
+// requires: jquery, util
+
+if(!jQluster) { jQluster = {}; }
+
+(function(my, $) {
+    var myclass = my.ReadinessCallbackManager = function(args) {
+        // @params: args.transport, args.notify = []
+        var self = this;
+        var doc_factories;
+        if(!my.defined(args.transport)) {
+            throw "transport parameter is mandatory";
+        }
+        if(!my.defined(args.notify)) args.notify = [];
+        if(!$.isArray(args.notify)) args.notify = [args.notify];
+
+        self.transport = args.transport;
+        self.notify_listeners_for = {};
+        self.notified_dict = {};
+        
+        doc_factories = $(document).data("jqluster-readiness-callback-managers") || {};
+        doc_factories[self.transport.getMyRemoteID()] = self;
+        $(document).data("jqluster-readiness-callback-managers", doc_factories);
+
+        self._notify(args.notify);
+    };
+    myclass.release = function() {
+        $(document).data("jqluster-readiness-callback-managers", {});
+    };
+    myclass.prototype = {
+        _notify: function(notified_remote_id_array) {
+            var self = this;
+            $.each(notified_remote_id_array, function(i, notified_remote_id) {
+                self.notified_dict[notified_remote_id] = true;
+                self.transport.selectAndGet({
+                    remote_id: notified_remote_id,
+                    eval_code: '$(document).data("jqluster-readiness-callback-managers")['+ my.quoteString(notified_remote_id) +']._beNotified('
+                        + my.quoteString(self.transport.getMyRemoteID()) +')'
+                });
+            });
+        },
+        _isNotifying: function(notified_remote_id) {
+            // @return: true if this Factory notifies its readiness to notified_remote_id. false otherwise.
+            return this.notified_dict[notified_remote_id] || false;
+        },
+        _beNotified: function(from_remote_id) {
+            var self = this;
+            var listeners = self.notify_listeners_for[from_remote_id];
+            if(!listeners) return;
+            $.each(listeners, function(i, callback) {
+                callback();
+            });
+        },
+        _checkIfRemoteAvailable: function(remote_id) {
+            // @return: promise, resolved if the remote is available, rejected if not.
+            var self = this;
+            var result_d = $.Deferred();
+            self.transport.selectAndGet({
+                remote_id: remote_id,
+                eval_code: '$(document).data("jqluster-readiness-callback-managers")['+ my.quoteString(remote_id) +']._isNotifying('
+                    + my.quoteString(self.transport.getMyRemoteID()) +')'
+            }).then(function(does_remote_notify_you) {
+                if(does_remote_notify_you) {
+                    result_d.resolve();
+                }else {
+                    result_d.reject("remote node exists, but it does not notify you.");
+                }
+            }, function() {
+                result_d.reject("remote node does not exist or network error.");
+            });
+            return result_d.promise();
+        },
+        listenToRemoteReadiness: function(remote_id, callback) {
+            // @return nothing
+            var self = this;
+            if(!self.notify_listeners_for[remote_id]) {
+                self.notify_listeners_for[remote_id] = [];
+            }
+            self.notify_listeners_for[remote_id].push(callback);
+            self._checkIfRemoteAvailable(remote_id).then(function() {
+                callback();
+            });
+        },
+    };
+})(jQluster, jQuery);
--- js/remote_selector_factory.js
+++ js/remote_selector_factory.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // jQluster factory object for generating RemoteSelector objects
-// requires: jquery, util, remote_selector, transport, local_server
+// requires: jquery, util, remote_selector, transport, local_server, loopback_transport, readiness_callback_manager
 
 if(!jQluster) { var jQluster = {}; }
 
@@ -9,31 +9,26 @@ if(!jQluster) { var jQluster = {}; }
     var local_server;
     var myclass = my.RemoteSelectorFactory = function(args) {
         // @params: args.my_remote_id, args.transport_id, args.notify = []
-        var doc_factories;
         if(!my.defined(args.my_remote_id)) {
             throw "my_remote_id parameter is mandatory";
         }
         if(!my.defined(args.transport_id)) {
             throw "transport_id parameter is mandatory";
         }
-        if(!my.defined(args.notify)) args.notify = [];
-        if(!$.isArray(args.notify)) args.notify = [args.notify];
-        
         this.transport = myclass._createTransport(args.my_remote_id, args.transport_id);
-        this.notify_listeners_for = {};
-        this.notified_dict = {};
-        
-        doc_factories = $(document).data("jqluster-factories") || {};
-        doc_factories[args.my_remote_id] = this;
-        $(document).data("jqluster-factories", doc_factories);
-
-        this._notify(args.notify);
+        this.readiness_callback_manager = new my.ReadinessCallbackManager({
+            transport: this.transport, notify: args.notify
+        });
     };
     myclass._createTransport = function(my_remote_id, transport_id) {
-        return new my.Transport({
-            remote_id: my_remote_id,
-            connection_object: myclass._createConnection(transport_id)
-        });
+        if(transport_id === "loopback") {
+            return new my.LoopbackTransport();
+        }else {
+            return new my.Transport({
+                remote_id: my_remote_id,
+                connection_object: myclass._createConnection(transport_id)
+            });
+        }
     };
     myclass._createConnection = function(transport_id) {
         if(transport_id === "local") {
@@ -48,9 +43,6 @@ if(!jQluster) { var jQluster = {}; }
     };
     myclass.releaseLocalServer = function() {
         local_server = undefined;
-    };
-    myclass.releaseFactories = function() {
-        $(document).data("jqluster-factories", {});
     };
     myclass.prototype = {
         _createRemoteSelector: function(remote_id, target) {
@@ -78,41 +70,13 @@ if(!jQluster) { var jQluster = {}; }
             }
             return new my.RemoteSelector(args);
         },
-        _checkIfRemoteAvailable: function(remote_id) {
-            // @return: promise, resolved if the remote is available, rejected if not.
-            var self = this;
-            var result_d = $.Deferred();
-            self.transport.selectAndGet({
-                remote_id: remote_id,
-                eval_code: '$(document).data("jqluster-factories")['+ my.quoteString(remote_id) +'].isNotifying('
-                           + my.quoteString(self.transport.getMyRemoteID()) +')'
-            }).then(function(does_remote_notify_you) {
-                if(does_remote_notify_you) {
-                    result_d.resolve();
-                }else {
-                    result_d.reject("remote node exists, but it does not notify you.");
-                }
-            }, function() {
-                result_d.reject("remote node does not exist or network error.");
-            });
-            return result_d.promise();
-        },
-        _listenToRemoteReadiness: function(remote_id, callback, factory_func) {
-            var self = this;
-            var wrapped_callback = function() { callback(factory_func) };
-            if(!self.notify_listeners_for[remote_id]) {
-                self.notify_listeners_for[remote_id] = [];
-            }
-            self.notify_listeners_for[remote_id].push(wrapped_callback);
-            self._checkIfRemoteAvailable(remote_id).then(function() {
-                wrapped_callback();
-            });
-        },
         forRemote: function(remote_id, immediate_target) {
             var self = this;
             var factory = function(target) {
                 if($.isFunction(target)) {
-                    self._listenToRemoteReadiness(remote_id, target, factory);
+                    self.readiness_callback_manager.listenToRemoteReadiness(
+                        remote_id, function() { target(factory); }
+                    );
                 }else {
                     return self._createRemoteSelector(remote_id, target);
                 }
@@ -122,29 +86,6 @@ if(!jQluster) { var jQluster = {}; }
             }else {
                 return factory;
             }
-        },
-        _notify: function(notified_remote_id_array) {
-            var self = this;
-            $.each(notified_remote_id_array, function(i, notified_remote_id) {
-                self.notified_dict[notified_remote_id] = true;
-                self.transport.selectAndGet({
-                    remote_id: notified_remote_id,
-                    eval_code: '$(document).data("jqluster-factories")['+ my.quoteString(notified_remote_id) +'].beNotified('
-                               + my.quoteString(self.transport.getMyRemoteID()) +')'
-                });
-            });
-        },
-        isNotifying: function(notified_remote_id) {
-            // @return: true if this Factory notifies its readiness to notified_remote_id. false otherwise.
-            return this.notified_dict[notified_remote_id] || false;
-        },
-        beNotified: function(from_remote_id) {
-            var self = this;
-            var listeners = self.notify_listeners_for[from_remote_id];
-            if(!listeners) return;
-            $.each(listeners, function(i, callback) {
-                callback();
-            });
         },
     };
 })(jQluster, jQuery);
