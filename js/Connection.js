61033cb68df533c41b46a51123154e2b3b0a7541
Toshio Ito <debug.ito@gmail.com>
2013-12-22 17:49:01 +0900

    Write JSDoc for ConnectionLocal.js

--- js/Connection.js
+++ js/Connection.js
@@ -1,5 +1,10 @@
 "use strict";
 
+/**
+ * @file
+ * @author Toshio Ito
+ */
+
 /** @namespace jQluster */
 if(!jQluster) { var jQluster = {}; }
 
@@ -12,7 +17,6 @@ if(!jQluster) { var jQluster = {}; }
      * object is supposed to be contained by Transport object.
      *
      * @requires jQuery
-     * @author Toshio Ito
      */
     var myclass = my.Connection = function() {
         this.receive_callbacks = [];
@@ -24,19 +28,26 @@ if(!jQluster) { var jQluster = {}; }
         },
         /**
          * Send data via this Connection.
+         * @param {jQluster.Connection~Message} message
          * @abstract
          */
         send: function() { throw "send() must be implemented"; },
+
+        /**
+         * A message object that is exchanged via Connection.
+         * @typedef {Object} jQluster.Connection~Message
+         * @see doc/protocol.md in jQluster package.
+         */
         
         /** 
          * A callback that is called when some data is received via this Connection.
-         * @callback jQluster.Connection~receiveCallback
-         * @param message
+         * @callback jQluster.Connection~ReceiveCallback
+         * @param {jQluster.Connection~Message} message
          */
         
         /**
          * Register receiveCallback
-         * @param {jQluster.Connection~receiveCallback} callback
+         * @param {jQluster.Connection~ReceiveCallback} callback
          * @returns nothing
          */
         onReceive: function(callback) {
@@ -45,7 +56,6 @@ if(!jQluster) { var jQluster = {}; }
 
         /**
          * Tell the Connection some data is arrived.
-         * @protected
          * @returns nothing
          */
         triggerReceive: function(message) {
@@ -55,5 +65,3 @@ if(!jQluster) { var jQluster = {}; }
         },
     };
 })(jQluster, jQuery);
-
-
--- js/ConnectionLocal.js
+++ js/ConnectionLocal.js
@@ -1,42 +1,59 @@
 "use strict";
 
-// Local connection implementation for testing and loopback transport.
-// requires: jquery, util.js, Connection.js
+/**
+ * @file
+ * @author Toshio Ito
+ */
 
+/** @namespace jQluster */
 if(!jQluster) { var jQluster = {}; }
 
 (function(my, $){
     var superclass = my.Connection;
 
-    // jQluster Connection implementation specifically to the ServerLocal object.
+    /**
+     * @class
+     * @alias jQluster.ConnectionLocal
+     * @extends jQluster.Connection
+     *
+     * @classdesc Local connection implementation for testing and loopback transport.
+     * @requires jQuery
+     * @requires util.js
+     * @requires Connection.js
+     *
+     * @param {jQluster.ServerLocal} server - local server object.
+     */
     my.ConnectionLocal = function(server) {
         superclass.apply(this);
         this.server = server;
         this.log = [];
     };
-    my.ConnectionLocal.prototype = $.extend({}, superclass.prototype, {
-        send: function(message) {
-            // @return: nothing
-            this.log.push({ direction: "send",  message: my.clone(message)});
-            // console.log("send: type: " + message.message_type + ", from: " + message.from);
-            if(message.message_type === "register") {
-                this.server.register(this, message.body.node_id, message.message_id);
-            }else {
-                this.server.distribute(message);
-            }
-        },
-        triggerReceive: function(message) {
-            this.log.push({ direction: "receive", message: my.clone(message)});
-            return superclass.prototype.triggerReceive.call(this, message);
-        },
-        release: function() {
-            superclass.prototype.release.call(this);
-            this.server = null;
-            this.log = [];
-        },
+    my.ConnectionLocal.prototype = $.extend(
+        {}, superclass.prototype,
+        /** @lends jQluster.ConnectionLocal.prototype */
+        {
+            send: function(message) {
+                this.log.push({ direction: "send",  message: my.clone(message)});
+                // console.log("send: type: " + message.message_type + ", from: " + message.from);
+                if(message.message_type === "register") {
+                    this.server.register(this, message.body.node_id, message.message_id);
+                }else {
+                    this.server.distribute(message);
+                }
+            },
+            triggerReceive: function(message) {
+                this.log.push({ direction: "receive", message: my.clone(message)});
+                return superclass.prototype.triggerReceive.call(this, message);
+            },
+            release: function() {
+                superclass.prototype.release.call(this);
+                this.server = null;
+                this.log = [];
+            },
 
-        // below are ConnectionLocal specific functions
-        getLog: function() { return this.log; },
-        clearLog: function() { this.log = [] }
-    });
+            // below are ConnectionLocal specific functions
+            getLog: function() { return this.log; },
+            clearLog: function() { this.log = [] }
+        }
+    );
 })(jQluster, jQuery);
