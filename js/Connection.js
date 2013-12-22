805157968160482e58823e34f6cfdf56e0a4866a
Toshio Ito <debug.ito@gmail.com>
2013-12-22 17:15:05 +0900

    Write JSDoc for util.js and Connection.js

--- js/Connection.js
+++ js/Connection.js
@@ -1,27 +1,54 @@
 "use strict";
 
-// jQluster abstract "Connection" class. A Connection object is
-// supposed to be contained by Transport object.
-
-// requires: jquery
-
+/** @namespace jQluster */
 if(!jQluster) { var jQluster = {}; }
 
+
 (function(my, $) {
+    /**
+     * @class 
+     * @alias jQluster.Connection
+     * @classdesc jQluster abstract "Connection" class. A Connection
+     * object is supposed to be contained by Transport object.
+     *
+     * @requires jQuery
+     * @author Toshio Ito
+     */
     var myclass = my.Connection = function() {
         this.receive_callbacks = [];
     };
     myclass.prototype = {
+        /** Safely release the Connection's resource. */
         release: function() {
             this.receive_callbacks = [];
         },
+        /**
+         * Send data via this Connection.
+         * @abstract
+         */
         send: function() { throw "send() must be implemented"; },
+        
+        /** 
+         * A callback that is called when some data is received via this Connection.
+         * @callback jQluster.Connection~receiveCallback
+         * @param message
+         */
+        
+        /**
+         * Register receiveCallback
+         * @param {jQluster.Connection~receiveCallback} callback
+         * @returns nothing
+         */
         onReceive: function(callback) {
-            // @return: nothing
             this.receive_callbacks.push(callback);
         },
+
+        /**
+         * Tell the Connection some data is arrived.
+         * @protected
+         * @returns nothing
+         */
         triggerReceive: function(message) {
-            // @return: nothing
             $.each(this.receive_callbacks, function(i, callback) {
                 callback(message);
             });
--- js/util.js
+++ js/util.js
@@ -16,6 +16,7 @@ if(!jQluster) { var jQluster = {}; }
      * @lends jQluster
      */
     var utils = {
+        /** Create a UUID string */
         uuid: function() {
             // http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript
             return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
@@ -23,12 +24,15 @@ if(!jQluster) { var jQluster = {}; }
                 return v.toString(16);
             });
         },
+        /** Convert 'arguments' object into a plain Array. */
         argsToArray: function(arguments_object) {
             return Array.prototype.slice.call(arguments_object, 0);
         },
+        /** Clone a plain Object. */
         clone: function(obj) {
             return $.extend(true, {}, obj);
         },
+        /** Stringify obj into JSON. Some dangerous elements in obj are ignored. */
         JSONstringifySafely: function(obj) {
             return JSON.stringify(obj, function(key, value) {
                 if($.isWindow(value) || value === document || this === value || my.isHTMLElement(value)) {
@@ -38,19 +42,23 @@ if(!jQluster) { var jQluster = {}; }
                 }
             });
         },
+        /** Clone obj via JSON (stringify -> parse). This simulates network communication using JSON. */
         cloneViaJSON: function(obj) {
             return JSON.parse(my.JSONstringifySafely(obj));
         },
 
+        /** @returns true if val is not null or undefined. */
         defined: function(val) {
             return (val !== null && typeof(val) !== 'undefined');
         },
 
+        /** @returns XPath string for the given jQuery object. */
         xpathFor: function($jquery_object) {
             // https://github.com/bimech/ellocate.js
             return $jquery_object.ellocate().xpath;
         },
 
+        /** @returns true of obj looks like HTML Element. */
         isHTMLElement: function(obj) {
             // ** http://stackoverflow.com/questions/384286/javascript-isdom-how-do-you-check-if-a-javascript-object-is-a-dom-object
             return (typeof HTMLElement === "object"
@@ -58,10 +66,12 @@ if(!jQluster) { var jQluster = {}; }
                     : obj && typeof obj === "object" && obj !== null && obj.nodeType === 1 && typeof obj.nodeName==="string");
         },
 
+        /** @returns a quoted version of str. */
         quoteString: function(str) {
             return JSON.stringify("" + str);
         },
 
+        /** @returns a string that is a serialized version of the given 'arguments' object. */
         argumentsStringFor: function(args) {
             return $.map(my.argsToArray(args), function(arg) {
                 return arg === null ? "null"
